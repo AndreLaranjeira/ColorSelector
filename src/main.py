cd62dd32c932eb6b7e05aa8fa7c36788ef309bdf
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2019-08-21 09:24:08 -0300

    Argument parser * Created an argument parser for the program, adding one argument for image files, one for video files and one for the webcam video stream. * Modified the program to use the argument parser instead of the options menus (right now, the behavior for each argument is still a placeholder).

--- src/main.py
+++ src/main.py
@@ -1,29 +1,37 @@
 # Program to highlight the color of a pixel selected in an image or video.
 
 # Libraries:
+import argparse
 import cv2 as cv
 import numpy as np
 
 # User modules:
 
+# Argument parser:
+parser = argparse.ArgumentParser(description='Color selector in image and video.')
+parser.add_argument('-i', type=str, dest='image',
+                    help="path to a jpg image file.")
+parser.add_argument('-v', type=str, dest='video',
+                    help="path to an avi video file.")
+parser.add_argument('-w', dest='webcam', action='store_true', default=False,
+                    help="use webcam input as video source.")
+args = parser.parse_args()
+
 # Main function:
-print("Please select one of the options below: ")
-print("")
-print("1 - Open an image file (default).")
-print("2 - Open a video file.")
-print("3 - Use the video feed from the webcam.")
-print("")
 
-option = int(input('Option: '))
+## Image:
+if(args.image is not None and args.video is None and args.webcam == False):
+    print("Placeholder for image.")
 
-if(option == 1):
-    print("Placeholder.")
 
-elif(option == 2):
-    print("Placeholder.")
+## Video:
+elif(args.image is None and args.video is not None and args.webcam == False):
+    print("Placeholder for video.")
 
-elif(option == 3):
-    print("Placeholder.")
+## Webcam:
+elif(args.image is None and args.video is None and args.webcam == True):
+    print("Placeholder for webcam.")
 
+## Invalid arguments:
 else:
-    print("Placeholder.")
+    print("Invalid program usage! Please use only one argument at a time!")
