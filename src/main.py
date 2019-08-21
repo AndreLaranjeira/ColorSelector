bfd4c6b0f1c1d9810a5fb368bf84e60817aa2de0
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2019-08-21 09:35:41 -0300

    Requirement 01 - Pixel information * Program can now open an image and display the position and color information of a pixel that has been clicked. * Minor changes to error messages.

--- src/main.py
+++ src/main.py
@@ -17,12 +17,64 @@ parser.add_argument('-w', dest='webcam', action='store_true', default=False,
                     help="use webcam input as video source.")
 args = parser.parse_args()
 
+# Auxiliary functions:
+def isImageGreyscale(img_shape):
+    if(len(img_shape) == 3 and img_shape[2] != 1):
+        return False
+    else:
+        return True
+
+def mouseEventHandler(event, x, y, flags, params):
+    if(event == cv.EVENT_LBUTTONDOWN):
+        printPixelInfo(row=y, col=x)
+
+def printPixelInfo(row, col):
+    print("Pixel information:")
+    print("Row: ", row)
+    print("Column: ", col)
+
+    pixel = img[row, col]
+
+    if(is_img_greyscale):
+        print("Greyscale value: ", pixel)
+
+    else:
+        B, G, R = pixel
+        print("Blue value: ", B)
+        print("Green value: ", G)
+        print("Red value: ", R)
+
+    print("")
+
+
 # Main function:
 
 ## Image:
 if(args.image is not None and args.video is None and args.webcam == False):
-    print("Placeholder for image.")
 
+    # Load image:
+    img = cv.imread(args.image)
+
+    # Check if an image was loaded:
+    if(img is None):
+        print("Image not found!")
+
+    else:
+        # Determine if the image has color:
+        is_img_greyscale = isImageGreyscale(img.shape)
+
+        # Configure image window:
+        cv.imshow("ColorSelector", img)
+        cv.setMouseCallback("ColorSelector", mouseEventHandler)
+
+        # Wait for ESC key to be pressed:
+        while(1):
+            key = cv.waitKey(100) & 0xFF
+            if(key == 27):
+                break
+
+        # Clean up resources:
+        cv.destroyAllWindows()
 
 ## Video:
 elif(args.image is None and args.video is not None and args.webcam == False):
@@ -34,4 +86,4 @@ elif(args.image is None and args.video is None and args.webcam == True):
 
 ## Invalid arguments:
 else:
-    print("Invalid program usage! Please use only one argument at a time!")
+    print("Invalid program usage! Please use exactly one argument!")
