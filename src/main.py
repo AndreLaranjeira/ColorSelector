0f5a1d5d139179ffe81a2268dd4308db65ed8c92
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2019-08-22 05:14:01 -0300

    Requirement 02 - Pixel highlighting * Program can now highlight pixels based on the color of the pixel clicked by the user. * Program can now clear image with the press of the 'c' key.

--- src/main.py
+++ src/main.py
@@ -18,6 +18,33 @@ parser.add_argument('-w', dest='webcam', action='store_true', default=False,
 args = parser.parse_args()
 
 # Auxiliary functions:
+def highlightInImage(img, color):
+    result = np.copy(img)
+
+    # Create auxiliary variables to stop integer overflow in subtractions:
+    aux_img = np.array(img, dtype=np.int32)
+    aux_color = np.array(color, dtype=np.int32)
+
+    # Calculate distance:
+    distance = (aux_img - aux_color) ** 2
+
+    # Greyscale logic for an image with 3 channels:
+    if(is_img_greyscale and len(img.shape) == 3):
+        mask = np.where((distance[:,:,0] < (13 ** 2)), True, False)
+        result[(mask)] = [255]
+
+    # Greyscale logic for an image with 2 channels:
+    elif(is_img_greyscale and len(img.shape) == 2):
+        mask = np.where((distance < (13 ** 2)), True, False)
+        result[(mask)] = [255]
+
+    # Color logic:
+    else:
+        mask = np.where(((distance[:,:,0] + distance[:,:,1] + distance[:,:,2]) < (13 ** 2)), True, False)
+        result[(mask)] = [0, 0, 255]
+
+    return result
+
 def isImageGreyscale(img_shape):
     if(len(img_shape) == 3 and img_shape[2] != 1):
         return False
@@ -26,20 +53,25 @@ def isImageGreyscale(img_shape):
 
 def mouseEventHandler(event, x, y, flags, params):
     if(event == cv.EVENT_LBUTTONDOWN):
-        printPixelInfo(row=y, col=x)
+        pixel_row = y
+        pixel_col = x
+        pixel_color = img[pixel_row, pixel_col]
+        printPixelInfo(row=pixel_row, col=pixel_col, color=pixel_color)
 
-def printPixelInfo(row, col):
+        # Add pixel color to highlighted colors:
+        highlighted_color.clear()
+        highlighted_color.append(pixel_color)
+
+def printPixelInfo(row, col, color):
     print("Pixel information:")
     print("Row: ", row)
     print("Column: ", col)
 
-    pixel = img[row, col]
-
     if(is_img_greyscale):
-        print("Greyscale value: ", pixel)
+        print("Greyscale value: ", color)
 
     else:
-        B, G, R = pixel
+        B, G, R = color
         print("Blue value: ", B)
         print("Green value: ", G)
         print("Red value: ", R)
@@ -67,11 +99,28 @@ if(args.image is not None and args.video is None and args.webcam == False):
         cv.imshow("ColorSelector", img)
         cv.setMouseCallback("ColorSelector", mouseEventHandler)
 
-        # Wait for ESC key to be pressed:
+        # Initiate the color variable:
+        highlighted_color = []
+
+        # Main event loop:
         while(1):
+
+            # Highlight selected color in image:
+            if(highlighted_color):
+                new_img = highlightInImage(img, highlighted_color[0])
+                cv.imshow("ColorSelector", new_img)
+                highlighted_color.clear()
+
+            # Read user input:
             key = cv.waitKey(100) & 0xFF
+
+            # Exit application:
             if(key == 27):
                 break
+
+            # Clean image:
+            elif(key == ord('c')):
+                cv.imshow("ColorSelector", img)
 
         # Clean up resources:
         cv.destroyAllWindows()
